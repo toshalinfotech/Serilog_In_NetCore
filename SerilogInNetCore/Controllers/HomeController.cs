bbb8ca0d006fb8a514f58c5abbc931d877f6fe48
Varun <varun@idea2working.com>
2019-10-12 16:14:47 +0530

    initial commit.

---
+++ SerilogInNetCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SerilogInNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            LogHelper.Debug("Test Debug");
+            LogHelper.Info("Test Info");
+            LogHelper.Error("Test Error");
+
+            try
+            {
+                string test = null;
+                test.ToLower();
+            }
+            catch (System.Exception ex)
+            {
+                LogHelper.Error(ex, "Error in Home Controller");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SerilogInNetCore/Helper/LogHelper.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+
+namespace SerilogInNetCore
+{
+    public class LogHelper
+    {
+        public static void Debug(string message)
+        {
+            if (Log.IsEnabled(Serilog.Events.LogEventLevel.Debug))
+                Log.Debug(message);
+        }
+
+        public static void Error(string message)
+        {
+            if (Log.IsEnabled(Serilog.Events.LogEventLevel.Error))
+                Log.Error(message);
+        }
+
+        public static void Error(System.Exception exception, string message)
+        {
+            if (Log.IsEnabled(Serilog.Events.LogEventLevel.Error))
+                Log.Error(exception, message);
+        }
+
+        public static void Info(string message)
+        {
+            if (Log.IsEnabled(Serilog.Events.LogEventLevel.Information))
+                Log.Information(message);
+        }
+
+        public static void Warning(string message)
+        {
+            if (Log.IsEnabled(Serilog.Events.LogEventLevel.Warning))
+                Log.Warning(message);
+        }
+    }
+}
